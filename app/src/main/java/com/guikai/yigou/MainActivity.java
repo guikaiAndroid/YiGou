aa4a9ac430bf73abdcc2579eab29a8e96f4d4edf
guikai <916126728@qq.com>
2019-01-23 23:24:29 +0800

    注册UI验证

--- app/src/main/java/com/guikai/yigou/MainActivity.java
+++ app/src/main/java/com/guikai/yigou/MainActivity.java
@@ -1,13 +1,27 @@
 package com.guikai.yigou;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
 import com.guikai.latte.activities.ProxyActivity;
 import com.guikai.latte.fragments.LatteFragment;
 import com.guikai.latte.launcher.LauncherFragment;
+import com.guikai.latte.sign.SignUpFragment;
 
 public class MainActivity extends ProxyActivity {
 
     @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null) {
+            actionBar.hide();
+        }
+    }
+
+    @Override
     public LatteFragment setRootFragment() {
-        return new LauncherFragment();
+        return new SignUpFragment();
     }
 }
---
+++ latte-ec/src/main/java/com/guikai/latte/sign/SignUpFragment.java
@@ -0,0 +1,125 @@
+package com.guikai.latte.sign;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.guikai.latte.fragments.LatteFragment;
+import com.guikai.latte.net.RestClient;
+import com.guikai.latte.net.callback.ISuccess;
+import com.guikai.latteec.R;
+
+/**
+ * Created by Anding on 2019/1/23 22:38
+ * Note:注册界面
+ */
+
+public class SignUpFragment extends LatteFragment {
+
+    private TextInputEditText mName = null;
+    private TextInputEditText mEmail = null;
+    private TextInputEditText mPhone = null;
+    private TextInputEditText mPassword = null;
+    private TextInputEditText mRePassword = null;
+
+    @Override
+    public Object setLayout() {
+        return R.layout.fragment_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View root) {
+        mName = $(R.id.edit_sign_up_name);
+        mEmail = $(R.id.edit_sign_up_email);
+        mPhone = $(R.id.edit_sign_up_phone);
+        mPassword = $(R.id.edit_sign_up_password);
+        mRePassword = $(R.id.edit_sign_up_re_password);
+
+        $(R.id.btn_sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSignUp();
+            }
+        });
+
+        $(R.id.tv_link_sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickLink();
+            }
+        });
+    }
+
+    private void onClickSignUp() {
+        if (checkForm()) {
+//            RestClient.builder()
+//                    .url("sign_up")
+//                    .params("","")
+//                    .success(new ISuccess() {
+//                        @Override
+//                        public void onSuccess(String response) {
+//
+//                        }
+//                    })
+//                    .build()
+//                    .post();
+            Toast.makeText(getContext(),"验证通过",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void onClickLink() {
+
+    }
+
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePassword.getText().toString();
+
+        boolean isPass = true;
+
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePassword.setError("两次密码输入不一致");
+            isPass = false;
+        } else {
+            mRePassword.setError(null);
+        }
+
+        return isPass;
+    }
+
+}
