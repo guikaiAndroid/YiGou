06ff5e4674066951b0508d15f8da8bae9934d493
Gui-kai <916126728@qq.com>
2019-02-12 11:02:55 +0800

    按照线上api进行开发 自测json必要时在释放

--- latte-ec/src/main/java/com/guikai/latte/main/index/IndexDataConverter.java
+++ latte-ec/src/main/java/com/guikai/latte/main/index/IndexDataConverter.java
@@ -15,13 +15,69 @@ import java.util.ArrayList;
  */
 public class IndexDataConverter extends DataConverter {
 
+//    @Override
+//    public ArrayList<MultipleItemEntity> convert() {
+//        final JSONArray dataArray = JSON.parseObject(getmJsonData()).getJSONArray("data");
+//        final int size = dataArray.size();
+//        for (int i = 0; i < size; i++) {
+//            final JSONObject data = dataArray.getJSONObject(i);
+//            final int itemType = data.getInteger("type");
+//            final String imageUrl = data.getString("imageUrl");
+//            final String text = data.getString("text");
+//            final int spanSize = data.getInteger("spanSize");
+//            final int id = data.getInteger("goodsId");
+//            final JSONArray banners = data.getJSONArray("banners");
+//
+//            final ArrayList<String> bannerImages = new ArrayList<>();
+//            int type = 0;
+//            switch (itemType) {
+//                case 1:
+//                    type = ItemType.BANNER;
+//                    //banner初始化
+//                    final int bannerSize = banners.size();
+//                    for (int j = 0; j < bannerSize; j++) {
+//                        final String banner = banners.getString(j);
+//                        bannerImages.add(banner);
+//                    }
+//                    break;
+//                case 2:
+//                    break;
+//                case 3:
+//                    type = ItemType.IMAGE;
+//                    break;
+//                case 4:
+//                    type = ItemType.TEXT_IMAGE;
+//                    break;
+//                case 5:
+//                    break;
+//                case 6:
+//                    break;
+//                case 7:
+//                    type = ItemType.TEXT;
+//                    break;
+//                default:
+//                    break;
+//            }
+//
+//            final MultipleItemEntity entity = MultipleItemEntity.builder()
+//                    .setField(MultipleFields.ITEM_TYPE, type)
+//                    .setField(MultipleFields.SPAN_SIZE, spanSize)
+//                    .setField(MultipleFields.ID, id)
+//                    .setField(MultipleFields.TEXT, text)
+//                    .setField(MultipleFields.IMAGE_URL, imageUrl)
+//                    .setField(MultipleFields.BANNERS, bannerImages)
+//                    .build();
+//            ENTITIES.add(entity);
+//        }
+//        return ENTITIES;
+//    }
+
     @Override
     public ArrayList<MultipleItemEntity> convert() {
         final JSONArray dataArray = JSON.parseObject(getmJsonData()).getJSONArray("data");
         final int size = dataArray.size();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i<size;i++) {
             final JSONObject data = dataArray.getJSONObject(i);
-            final int itemType = data.getInteger("type");
             final String imageUrl = data.getString("imageUrl");
             final String text = data.getString("text");
             final int spanSize = data.getInteger("spanSize");
@@ -30,43 +86,31 @@ public class IndexDataConverter extends DataConverter {
 
             final ArrayList<String> bannerImages = new ArrayList<>();
             int type = 0;
-            switch (itemType) {
-                case 1:
-                    type = ItemType.BANNER;
-                    //banner初始化
-                    final int bannerSize = banners.size();
-                    for (int j = 0; j < bannerSize; j++) {
-                        final String banner = banners.getString(j);
-                        bannerImages.add(banner);
-                    }
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    type = ItemType.IMAGE;
-                    break;
-                case 4:
-                    type = ItemType.TEXT_IMAGE;
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    type = ItemType.TEXT;
-                    break;
-                default:
-                    break;
+            if (imageUrl == null && text != null) {
+                type = ItemType.TEXT;
+            } else if (imageUrl != null && text == null){
+                type = ItemType.IMAGE;
+            } else if (imageUrl != null) {
+                type = ItemType.TEXT_IMAGE;
+            } else if (banners != null) {
+                type = ItemType.BANNER;
+                //banner初始化
+                final int bannerSize = banners.size();
+                for (int j=0 ;j<bannerSize;j++) {
+                    final String banner = banners.getString(j);
+                    bannerImages.add(banner);
+                }
             }
 
             final MultipleItemEntity entity = MultipleItemEntity.builder()
-                    .setField(MultipleFields.ITEM_TYPE, type)
-                    .setField(MultipleFields.SPAN_SIZE, spanSize)
-                    .setField(MultipleFields.ID, id)
-                    .setField(MultipleFields.TEXT, text)
-                    .setField(MultipleFields.IMAGE_URL, imageUrl)
-                    .setField(MultipleFields.BANNERS, bannerImages)
+                    .setField(MultipleFields.ITEM_TYPE,type)
+                    .setField(MultipleFields.SPAN_SIZE,spanSize)
+                    .setField(MultipleFields.ID,id)
+                    .setField(MultipleFields.TEXT,text)
+                    .setField(MultipleFields.IMAGE_URL,imageUrl)
+                    .setField(MultipleFields.BANNERS,bannerImages)
                     .build();
+
             ENTITIES.add(entity);
         }
         return ENTITIES;
