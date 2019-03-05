74b5c0b53edf3f96efdcf970bd109092db939080
Gui-kai <916126728@qq.com>
2019-03-05 21:02:13 +0800

    cart page

--- latte-ec/src/main/java/com/guikai/latte/main/EcBottomFragment.java
+++ latte-ec/src/main/java/com/guikai/latte/main/EcBottomFragment.java
@@ -8,6 +8,7 @@ import com.guikai.latte.fragments.bottom.BottomTabBean;
 import com.guikai.latte.fragments.bottom.BottomItemBuilder;
 import com.guikai.latte.main.discover.DiscoverFragment;
 import com.guikai.latte.main.index.IndexFragment;
+import com.guikai.latte.main.cart.ShopCartFragment;
 import com.guikai.latte.main.sort.SortFragment;
 
 import java.util.LinkedHashMap;
@@ -25,7 +26,7 @@ public class EcBottomFragment extends BaseBottomFragment{
         items.put(new BottomTabBean("{fa-home}","主页"), new IndexFragment());
         items.put(new BottomTabBean("{fa-sort}","分类"), new SortFragment());
         items.put(new BottomTabBean("{fa-compass}","发现"), new DiscoverFragment());
-        items.put(new BottomTabBean("{fa-shopping-cart}","购物车"), new IndexFragment());
+        items.put(new BottomTabBean("{fa-shopping-cart}","购物车"), new ShopCartFragment());
         items.put(new BottomTabBean("{fa-user}","我的"), new IndexFragment());
         return builder.addItems(items).build();
     }
--- latte-ec/src/main/java/com/guikai/latte/main/cart/ShopCartFragment.java
+++ latte-ec/src/main/java/com/guikai/latte/main/cart/ShopCartFragment.java
@@ -1,11 +1,15 @@
-package com.guikai.latte.main.shopCart;
+package com.guikai.latte.main.cart;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.guikai.latte.fragments.bottom.BottomItemFragment;
+import com.guikai.latteec.R;
+
+import static com.blankj.utilcode.util.BarUtils.getStatusBarHeight;
 
 /**
  * Created by Anding on 2019/1/27 18:22
@@ -15,11 +19,12 @@ import com.guikai.latte.fragments.bottom.BottomItemFragment;
 public class ShopCartFragment extends BottomItemFragment {
     @Override
     public Object setLayout() {
-        return null;
+        return R.layout.fragment_shop_cart;
     }
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View root) {
-
+        Toolbar mToolbar = $(R.id.tb_cart);
+        mToolbar.setPadding(0, getStatusBarHeight(),0,0);
     }
 }
