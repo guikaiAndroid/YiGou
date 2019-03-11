992b6c9726cfd6e875356e859438a81034c5d8fc
guikai <916126728@qq.com>
2019-03-11 23:40:03 +0800

    购物车数据测试

--- latte-ec/src/main/java/com/guikai/latte/main/cart/ShopCartFragment.java
+++ latte-ec/src/main/java/com/guikai/latte/main/cart/ShopCartFragment.java
@@ -6,7 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.guikai.latte.fragments.bottom.BottomItemFragment;
+import com.guikai.latte.net.RestClient;
+import com.guikai.latte.net.callback.ISuccess;
+import com.guikai.latte.util.log.LogUtils;
 import com.guikai.latteec.R;
 
 import static com.blankj.utilcode.util.BarUtils.getStatusBarHeight;
@@ -16,7 +20,7 @@ import static com.blankj.utilcode.util.BarUtils.getStatusBarHeight;
  * Note:
  */
 
-public class ShopCartFragment extends BottomItemFragment {
+public class ShopCartFragment extends BottomItemFragment implements ISuccess {
     @Override
     public Object setLayout() {
         return R.layout.fragment_shop_cart;
@@ -25,6 +29,22 @@ public class ShopCartFragment extends BottomItemFragment {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View root) {
         Toolbar mToolbar = $(R.id.tb_cart);
-        mToolbar.setPadding(0, getStatusBarHeight(),0,0);
+        mToolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        RestClient.builder()
+                .url("shop_cart.php")
+                .loader(getContext())
+                .success(this)
+                .build()
+                .get();
+    }
+
+    @Override
+    public void onSuccess(String response) {
+        ToastUtils.showLong(response);
     }
 }
