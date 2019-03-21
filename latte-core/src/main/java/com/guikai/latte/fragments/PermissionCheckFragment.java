9cab434843dc220f1acd1fdb53b7cb9438f7436a
Gui-kai <916126728@qq.com>
2019-03-21 16:41:21 +0800

    14-8 图片处理裁剪

--- latte-core/src/main/java/com/guikai/latte/fragments/PermissionCheckFragment.java
+++ latte-core/src/main/java/com/guikai/latte/fragments/PermissionCheckFragment.java
@@ -2,11 +2,19 @@ package com.guikai.latte.fragments;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 
 import com.blankj.utilcode.util.ToastUtils;
+import com.guikai.latte.ui.camera.CameraImageBean;
 import com.guikai.latte.ui.camera.LatteCamera;
+import com.guikai.latte.ui.camera.RequestCodes;
+import com.guikai.latte.util.callback.CallbackManager;
+import com.guikai.latte.util.callback.CallbackType;
+import com.guikai.latte.util.callback.IGlobalCallback;
+import com.yalantis.ucrop.UCrop;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
@@ -99,4 +107,47 @@ public abstract class PermissionCheckFragment extends BaseFragment {
         PermissionCheckFragmentPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RequestCodes.TAKE_PHOTO:
+                    final Uri resultUri = CameraImageBean.getInstance().getPath();
+                    if (getContext() != null) {
+                        UCrop.of(resultUri, resultUri)
+                                .withMaxResultSize(400, 400)
+                                .start(getContext(), this);
+                    }
+                    break;
+                case RequestCodes.PICK_PHOTO:
+                    if (data != null) {
+                        final Uri pickPath = data.getData();
+                        //从相册选择后需要有个路径存放裁剪后的图片
+                        final String pickCropPath = LatteCamera.createCropFile().getPath();
+                        if (pickPath != null && getContext() != null) {
+                            UCrop.of(pickPath, Uri.parse(pickCropPath))
+                                    .withMaxResultSize(400, 400)
+                                    .start(getContext(), this);
+                        }
+                    }
+                    break;
+                case RequestCodes.CROP_PHOTO:
+                    final Uri cropUri = UCrop.getOutput(data);
+                    //拿到裁剪后的数据进行处理
+                    @SuppressWarnings("unchecked") final IGlobalCallback<Uri> callback = CallbackManager
+                            .getInstance()
+                            .getCallback(CallbackType.ON_CROP);
+                    if (callback != null) {
+                        callback.executeCallback(cropUri);
+                    }
+                    break;
+                case RequestCodes.CROP_ERROR:
+                    ToastUtils.showShort("裁剪出错！");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
