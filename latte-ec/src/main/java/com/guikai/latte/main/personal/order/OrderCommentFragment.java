a2457f83ef7a07999b4073d04da2d7eebdec9a04
guikai <916126728@qq.com>
2019-03-27 00:06:58 +0800

    15-1 一键式评分控件设计与实现

--- latte-ec/src/main/java/com/guikai/latte/main/personal/order/OrderCommentFragment.java
+++ latte-ec/src/main/java/com/guikai/latte/main/personal/order/OrderCommentFragment.java
@@ -6,12 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.guikai.latte.fragments.LatteFragment;
+import com.guikai.latte.ui.widget.StarLayout;
 import com.guikai.latteec.R;
 
 import static com.blankj.utilcode.util.BarUtils.getStatusBarHeight;
 
 public class OrderCommentFragment extends LatteFragment {
+
+    private StarLayout mStarLayout = null;
+
+    void onClickSubmit() {
+        ToastUtils.showShort("评分：" + mStarLayout.getStarCount());
+    }
 
     @Override
     public Object setLayout() {
@@ -21,6 +29,13 @@ public class OrderCommentFragment extends LatteFragment {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View root) {
         Toolbar mToolbar = $(R.id.tb_order_comment);
+        mStarLayout = $(R.id.custom_star_layout);
         mToolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+        $(R.id.top_tv_comment_commit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSubmit();
+            }
+        });
     }
 }
